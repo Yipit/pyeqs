50ed16f8d90f2c6556a8c9bb2ff274d19ba4de68
Andrew Gross <andrew.w.gross@gmail.com>
2014-03-20 13:33:25 -0400

    Add ScriptScore test

---
+++ tests/unit/test_score.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from pyeqs.dsl import ScriptScore
+from tests.helpers import compare
+
+
+def test_add_score():
+    """
+    Create Score Block
+    """
+    # When add a Score field
+    t = ScriptScore("foo")
+
+    # Then I see the appropriate JSON
+    results = {
+        "script": "foo"
+    }
+
+    compare(t, results)
+
+
+def test_add_score_with_params():
+    """
+    Create Score Block with Params
+    """
+    # When add a Score field
+    t = ScriptScore("foo", params={"bar": "baz"})
+
+    # Then I see the appropriate JSON
+    results = {
+        "script": "foo",
+        "params": {
+            "bar": "baz"
+        }
+    }
+
+    compare(t, results)
+
+
+def test_add_score_with_lang():
+    """
+    Create Score Block with Languate
+    """
+    # When add a Score field
+    t = ScriptScore("foo", lang="mvel")
+
+    # Then I see the appropriate JSON
+    results = {
+        "script": "foo",
+        "lang": "mvel"
+    }
+
+    compare(t, results)
+
+
+def test_add_score_with_params_and_lang():
+    """
+    Create Score Block with Params and Lang
+    """
+    # When add a Score field
+    t = ScriptScore("foo", params={"bar": "baz"}, lang="mvel")
+
+    # Then I see the appropriate JSON
+    results = {
+        "script": "foo",
+        "params": {
+            "bar": "baz"
+        },
+        "lang": "mvel"
+    }
+
+    compare(t, results)
