1a482baa2721a4800daf51dd471a52376e8cbe1c
Andrew Gross <andrew.w.gross@gmail.com>
2014-03-24 11:03:04 -0400

    Add tests for filter

---
+++ tests/unit/test_filter.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from pyeqs import Filter
+from pyeqs.dsl import Term
+from tests.helpers import compare
+
+
+def test_create_filter():
+    """
+    Create Default Filter
+    """
+    # When create a filter block
+    t = Filter()
+
+    # Then I see the appropriate JSON
+    results = {
+        "and": []
+    }
+
+    compare(t, results)
+
+
+def test_add_filter():
+    """
+    Create Filter and Add Block
+    """
+    # When I create a filter
+    t = Filter()
+
+    # And add a block
+    t.filter(Term("foo", "bar"))
+
+    # Then I see the appropriate JSON
+    results = {
+        "and": [
+            {
+                "term": {
+                    "foo": "bar"
+                }
+            }
+        ]
+    }
+
+    compare(t, results)
+
+
+def test_filter_with_or():
+    """
+    Create OR Filter
+    """
+    # When create a filter block
+    t = Filter("or")
+
+    # Then I see the appropriate JSON
+    results = {
+        "or": []
+    }
+
+    compare(t, results)
+
+
+def test_filter_with_and():
+    """
+    Create AND Filter
+    """
+    # When create a filter block
+    t = Filter("and")
+
+    # Then I see the appropriate JSON
+    results = {
+        "and": []
+    }
+
+    compare(t, results)
