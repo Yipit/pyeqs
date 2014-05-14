2dde71bc79c70c5e326632a9a4a0487c5806fc88
Andrew Gross <andrew.w.gross@gmail.com>
2014-05-14 17:23:05 -0400

    Test simple filtered search

--- tests/functional/test_queryset.py
+++ tests/functional/test_queryset.py
@@ -4,13 +4,14 @@ from __future__ import unicode_literals
 from sure import scenario
 
 from pyeqs import QuerySet
+from pyeqs.dsl import Term
 from tests.helpers import prepare_data, cleanup_data, add_document
 
 
 @scenario(prepare_data, cleanup_data)
 def test_simple_search(context):
     """
-    Create Default QuerySet
+    Perform simple match_all search
     """
     # When create a queryset
     t = QuerySet("http://localhost:9200", index="foo")
@@ -20,6 +21,27 @@ def test_simple_search(context):
 
     # And I do a search
     results = t[0:1]
+
+    # Then I get a the expected results
+    len(results).should.equal(1)
+    results[0]['_source'].should.equal({"bar": "baz"})
+
+
+@scenario(prepare_data, cleanup_data)
+def test_simple_search_with_filter(context):
+    """
+    Perform simple filtered search
+    """
+    # When create a queryset
+    t = QuerySet("http://localhost:9200", index="foo")
+
+    # And there are records
+    add_document("foo", {"bar": "baz"})
+    add_document("foo", {"bar": "bazbaz"})
+
+    # And I do a filtered search
+    t.filter(Term("bar", "baz"))
+    results = t[0:10]
 
     # Then I get a the expected results
     len(results).should.equal(1)
