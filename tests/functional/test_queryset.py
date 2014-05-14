fb01dd37ba86481ca0e98b765131e4c317a91b27
Andrew Gross <andrew.w.gross@gmail.com>
2014-05-14 17:14:28 -0400

    Add simple functional test to retrieve all results

---
+++ tests/functional/test_queryset.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from sure import scenario
+
+from pyeqs import QuerySet
+from tests.helpers import prepare_data, cleanup_data, add_document
+
+
+@scenario(prepare_data, cleanup_data)
+def test_simple_search(context):
+    """
+    Create Default QuerySet
+    """
+    # When create a queryset
+    t = QuerySet("http://localhost:9200", index="foo")
+
+    # And there are records
+    add_document("foo", {"bar": "baz"})
+
+    # And I do a search
+    results = t[0:1]
+
+    # Then I get a the expected results
+    len(results).should.equal(1)
+    results[0]['_source'].should.equal({"bar": "baz"})
--- tests/helpers.py
+++ tests/helpers.py
@@ -3,6 +3,14 @@ from __future__ import unicode_literals
 
 import json
 
+from pyelasticsearch import (
+        ElasticSearch,
+        ElasticHttpNotFoundError
+)
+
+ELASTICSEARCH_URL = "http://localhost:9200"
+conn = ElasticSearch(ELASTICSEARCH_URL)
+
 
 def homogeneous(a, b):
     json.dumps(a).should.equal(json.dumps(b))
@@ -10,3 +18,37 @@ def homogeneous(a, b):
 
 def heterogeneous(a, b):
     json.dumps(a).shouldnt.equal(json.dumps(b))
+
+
+def add_document(index, document):
+    conn.index(index, "foo", document, id=None, refresh=True)
+
+
+def clean_elasticsearch(context):
+    _delete_es_index("foo")
+
+
+def prepare_elasticsearch(context):
+    clean_elasticsearch(context)
+    _create_foo_index()
+    conn.health(wait_for_status='yellow')
+
+
+def _create_foo_index():
+    conn.create_index("foo")
+
+
+def _delete_es_index(index):
+    try:
+        conn.delete_index(index)
+    except ElasticHttpNotFoundError:
+        pass
+
+
+prepare_data = [
+    prepare_elasticsearch
+]
+
+cleanup_data = [
+    clean_elasticsearch
+]
