492545a38cc9539649f205465bce67762044b8d6
Andrew Gross <andrew.w.gross@gmail.com>
2014-05-15 15:29:44 -0400

    Add mode sorting test

--- tests/functional/test_sorting.py
+++ tests/functional/test_sorting.py
@@ -54,3 +54,27 @@ def test_search_with_desc_sorting(context):
     results[0]["_source"]["bar"].should.equal(3)
     results[1]["_source"]["bar"].should.equal(2)
     results[2]["_source"]["bar"].should.equal(1)
+
+
+@scenario(prepare_data, cleanup_data)
+def test_search_with_mode_sorting(context):
+    """
+    Perform search with descending sorting and mode
+    """
+    # When create a query block
+    t = QuerySet("localhost", index="foo")
+
+    # And there are records
+    add_document("foo", {"bar": [1, 10]})
+    add_document("foo", {"bar": [2, 10]})
+    add_document("foo", {"bar": [3, 10]})
+
+    # And I add sorting
+    s = Sort("bar", order="desc", mode="min")
+    t.order_by(s)
+    results = t[0:10]
+
+    # Then my results have the proper sorting
+    results[0]["_source"]["bar"].should.equal([3, 10])
+    results[1]["_source"]["bar"].should.equal([2, 10])
+    results[2]["_source"]["bar"].should.equal([1, 10])
