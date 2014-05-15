1f722bba7f8dc0dccdc64e78b76307f4ffc85f01
Andrew Gross <andrew.w.gross@gmail.com>
2014-05-15 14:40:29 -0400

    Add terms test

--- tests/functional/test_queryset.py
+++ tests/functional/test_queryset.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 from sure import scenario
 
 from pyeqs import QuerySet, Filter
-from pyeqs.dsl import Term
+from pyeqs.dsl import Terms
 from tests.helpers import prepare_data, cleanup_data, add_document
 
 
@@ -25,3 +25,25 @@ def test_simple_search(context):
     # Then I get a the expected results
     len(results).should.equal(1)
     results[0]['_source'].should.equal({"bar": "baz"})
+
+
+@scenario(prepare_data, cleanup_data)
+def test_terms_search(context):
+    """
+    Perform simple terms filter
+    """
+    # When create a queryset
+    t = QuerySet("localhost", index="foo")
+
+    # And there are records
+    add_document("foo", {"bar": "baz", "foo": "foo"})
+    add_document("foo", {"bar": "bazbaz", "foo": "foo"})
+    add_document("foo", {"bar": "baz", "foo": "foofoo"})
+    add_document("foo", {"bar": "baz", "foo": "foofoofoo"})
+
+    # And I filter for terms
+    t.filter(Terms("foo", ["foo", "foofoo"]))
+    results = t[0:10]
+
+    # Then I get a the expected results
+    len(results).should.equal(3)
