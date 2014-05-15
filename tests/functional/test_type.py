8bebc12e3fa1e2f0a64736f773d10169c6778439
Andrew Gross <andrew.w.gross@gmail.com>
2014-05-15 15:38:59 -0400

    Add type test

---
+++ tests/functional/test_type.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from sure import scenario
+
+from pyeqs import QuerySet
+from pyeqs.dsl import Type
+from tests.helpers import prepare_data, cleanup_data, add_document
+
+
+@scenario(prepare_data, cleanup_data)
+def test_search_with_type(context):
+    """
+    Perform search with type filter
+    """
+    # When create a query block
+    t = QuerySet("localhost", index="foo")
+
+    # And there are records
+    add_document("foo", {"bar": 1})
+    add_document("foo", {"bar": 2})
+    add_document("foo", {"bar": 3}, doc_type="bar")
+
+    # And I add a type filter
+    _type = Type("bar")
+    t.filter(_type)
+    results = t[0:10]
+
+    # Then my results only have that type
+    len(results).should.equal(1)
+    results[0]["_source"]["bar"].should.equal(3)
