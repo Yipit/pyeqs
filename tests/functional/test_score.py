10b11b46b9e3b48b85db04041506313cd9f5bfe7
Andrew Gross <andrew.w.gross@gmail.com>
2014-05-19 11:37:36 -0400

    Add custom scoring

---
+++ tests/functional/test_score.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from sure import scenario
+
+from pyeqs import QuerySet
+from pyeqs.dsl import ScriptScore
+from tests.helpers import prepare_data, cleanup_data, add_document
+
+
+@scenario(prepare_data, cleanup_data)
+def test_search_with_scoring(context):
+    """
+    Search with custom scoring
+    """
+    # When create a query block
+    t = QuerySet("localhost", index="foo")
+
+    # And there are records
+    add_document("foo", {"bar": 1})
+    add_document("foo", {"bar": 2})
+    add_document("foo", {"bar": 3})
+
+    # And I add scoring
+    score = ScriptScore("s = 0 + doc['bar'].value")
+    t.score(score)
+    results = t[0:10]
+
+    # Then my results are scored correctly
+    len(results).should.equal(3)
+    results[0]["_source"]["bar"].should.equal(3)
+    results[1]["_source"]["bar"].should.equal(2)
+    results[2]["_source"]["bar"].should.equal(1)
+
+
+@scenario(prepare_data, cleanup_data)
+def test_search_with_scoring_and_lang(context):
+    """
+    Search with custom scoring and language
+    """
+    # When create a query block
+    t = QuerySet("localhost", index="foo")
+
+    # And there are records
+    add_document("foo", {"bar": 1})
+    add_document("foo", {"bar": 2})
+    add_document("foo", {"bar": 3})
+
+    # And I add scoring with a language
+    score = ScriptScore("s = 0 + doc['bar'].value", lang="mvel")
+    t.score(score)
+    results = t[0:10]
+
+    # Then my results are scored correctly
+    len(results).should.equal(3)
+    results[0]["_source"]["bar"].should.equal(3)
+    results[1]["_source"]["bar"].should.equal(2)
+    results[2]["_source"]["bar"].should.equal(1)
+
+
+@scenario(prepare_data, cleanup_data)
+def test_search_with_scoring_and_params(context):
+    """
+    Search with custom scoring and params
+    """
+    # When create a query block
+    t = QuerySet("localhost", index="foo")
+
+    # And there are records
+    add_document("foo", {"bar": 1})
+    add_document("foo", {"bar": 2})
+    add_document("foo", {"bar": 3})
+
+    # And I add scoring with params
+    score = ScriptScore("s = custom_param + doc['bar'].value", params={"custom_param": 1})
+    t.score(score)
+    results = t[0:10]
+
+    # Then my results are scored correctly
+    len(results).should.equal(3)
+    results[0]["_source"]["bar"].should.equal(3)
+    results[1]["_source"]["bar"].should.equal(2)
+    results[2]["_source"]["bar"].should.equal(1)
