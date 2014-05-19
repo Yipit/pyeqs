6fc00454957cec1f6635d13f02d12df4b2233a0f
Andrew Gross <andrew.w.gross@gmail.com>
2014-05-19 17:22:02 -0400

    Dont override range keyword

--- pyeqs/dsl/range.py
+++ pyeqs/dsl/range.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, absolute_import
 
+
 class Range(dict):
 
     def __init__(self, field_name, gte=None, gt=None, lte=None, lt=None):
@@ -13,15 +14,15 @@ class Range(dict):
         self["range"] = self._build_dict()
 
     def _build_dict(self):
-        range = {
+        _range = {
                 self.field_name: {}
             }
         if self.gte:
-            range[self.field_name]["gte"] = self.gte
+            _range[self.field_name]["gte"] = self.gte
         if self.gt:
-            range[self.field_name]["gt"] = self.gt
+            _range[self.field_name]["gt"] = self.gt
         if self.lte:
-            range[self.field_name]["lte"] = self.lte
+            _range[self.field_name]["lte"] = self.lte
         if self.lt:
-            range[self.field_name]["lt"] = self.lt
-        return range
+            _range[self.field_name]["lt"] = self.lt
+        return _range
