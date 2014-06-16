a3c91bf9829f6a02659617d857cfae1c08aa6a6f
Andrew Gross <andrew.w.gross@gmail.com>
2014-06-16 19:33:49 -0400

    Add note for python 3

--- setup.py
+++ setup.py
@@ -64,5 +64,7 @@ if __name__ == '__main__':
             'Programming Language :: Python',
             'Programming Language :: Python :: 2.6',
             'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
         )
     )
